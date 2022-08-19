a8b1418c819d5d9463a322af41b8c61c9903b03f
KoKeon1 <rjaakel1@naver.com>
2022-08-19 12:32:51 +0900

    JPA Setting

---
+++ src/main/java/com/kk/JPASturdy/JpaSturdyApplication.java
@@ -0,0 +1,13 @@
+package com.kk.JPASturdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSturdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSturdyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kk/JPASturdy/JpaSturdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.JPASturdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSturdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
